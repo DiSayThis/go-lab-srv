acfe36e93324dcef2c1b7ae8077f3b6b3deab736
Dmitriy <disaythis@gmail.com>
2026-07-15 18:02:38 +0300

    feat: default response /auth/login

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-api/configs"
 	"net/http"
@@ -22,6 +23,12 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 
 func (handler *AuthHandler) login(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("login", handler.Config.Auth.Secret)
+	res := LoginResponse{
+		Token: handler.Config.Auth.Secret,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(res)
 }
 
 func (handler *AuthHandler) register(w http.ResponseWriter, req *http.Request) {
